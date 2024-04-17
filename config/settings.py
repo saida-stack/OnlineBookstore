6161f6323c7876655b135ccdb7fa7400fc055900
Saida Moghimi <saidamoghimi@gmail.com>
2024-04-17 13:58:37 +0330

    add settings for adding covers

--- config/settings.py
+++ config/settings.py
@@ -145,5 +145,5 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
 # media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = [str(BASE_DIR.joinpath('media'))]
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 
